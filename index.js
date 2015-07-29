fae2e74b48127f458a0d68acec9b35a869f19646
ferni <fertiberti@gmail.com>
2015-07-29 20:32:42 -0300

    Generate first derived multisig address.

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 var coinkite = require('./coinkite');
+var bitcore = require('bitcore');
 
+var HDPrivateKey = bitcore.HDPrivateKey;
+var hdPrivateKey = new HDPrivateKey('xprv9s21ZrQH143K2vQPnZbpt5rdZoVUNsGSkvA5Usxmtf1sKu1hthgvc7U49DeixZHNAwPnQWq9hyCB37SdTddVYwyiEt7m6sJcrLMQp75QcBu');
+
+console.log('priv: ' + hdPrivateKey.xprivkey + '\n' + 'pub: ' + hdPrivateKey.hdPublicKey);
+var keys = [{priv: 'xpub661MyMwAqRbcGWgcbzgTxQXHz71cstnhGS54Ho6fRy39EaJFYbQBhUCXJm5wsiZtzAhajJScjJEFaiTSbfcAhbGbC8fKDRwyQz6utFup1UH'},
+    {
+    priv: 'xprv9s21ZrQH143K2vQPnZbpt5rdZoVUNsGSkvA5Usxmtf1sKu1hthgvc7U49DeixZHNAwPnQWq9hyCB37SdTddVYwyiEt7m6sJcrLMQp75QcBu',
+    pub: 'xpub661MyMwAqRbcFQUrtb8qFDoN7qKxnKzJ895gHGNPSzYrChLrSF1B9unXzWTHXYTcMTZrjsijWnbEJG3QjZ4Qdy9mdc56HQawfqFcVP8VxpW'
+
+
+},
+    {
+        priv: 'xprv9s21ZrQH143K27KwUhjnfmJH4jFZzCEqu1CkJzaa1FM9JexvBrmCxJ87p5VPqY8Uh7495zwyi62HSVCAwDMsL1F8qkFroDPQdiQ6iWdfadL',
+        pub: 'xpub661MyMwAqRbcEbQQajGo2uF1cm64PexhGE8M7NzBZat8BTJ4jQ5TW6SbfLfvfXvtViGNdJsrRe3Wkv4Y2ZpnVVPuTDEa3gek96dU5LunXKv'
+    }];
+var p1 = new bitcore.HDPublicKey('xpub661MyMwAqRbcGWgcbzgTxQXHz71cstnhGS54Ho6fRy39EaJFYbQBhUCXJm5wsiZtzAhajJScjJEFaiTSbfcAhbGbC8fKDRwyQz6utFup1UH');
+var p1a = p1.derive('m/0');
+var p2 = new bitcore.HDPublicKey('xpub661MyMwAqRbcFQUrtb8qFDoN7qKxnKzJ895gHGNPSzYrChLrSF1B9unXzWTHXYTcMTZrjsijWnbEJG3QjZ4Qdy9mdc56HQawfqFcVP8VxpW');
+var p2a = p2.derive('m/0');
+var p3 = new bitcore.HDPublicKey('xpub661MyMwAqRbcEbQQajGo2uF1cm64PexhGE8M7NzBZat8BTJ4jQ5TW6SbfLfvfXvtViGNdJsrRe3Wkv4Y2ZpnVVPuTDEa3gek96dU5LunXKv');
+var p3a = p3.derive('m/0');
+//console.log(JSON.stringify(p1a));
+
+
+var p2shAddress = new bitcore.Address([p1a.publicKey,
+    p2a.publicKey, p3a.publicKey], 2);
+console.log(p2shAddress.toString());
+process.exit();
 coinkite.request('/v1/my/self', 'GET', {}, function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
